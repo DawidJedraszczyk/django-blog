f2467f0c163b2002ed9ca3bf4c0150c6888cfaef
DawidJedraszczyk <dawidjedraszczyk01@gmail.com>
2024-11-29 00:07:15 +0100

    pagination in main view

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from apps.users.models import BlogUser
+from django.core.paginator import Paginator
+
 
 class PostsMixin(LoginRequiredMixin):
     login_url = reverse_lazy('account_login')
@@ -13,15 +15,27 @@ class PostsMixin(LoginRequiredMixin):
 
 class PostMainView(PostsMixin, View):
     template_name = 'posts/post_main.html'
+    paginate_by = 20
 
     def get_posts(self):
         return Post.objects.all().order_by('-created_at')
 
+    def paginate_posts(self, posts, page_number):
+        paginator = Paginator(posts, self.paginate_by)
+        return paginator.get_page(page_number)
+
     def get(self, request, *args, **kwargs):
         posts = self.get_posts()
+        page_number = request.GET.get('page', 1)
+        paginated_posts = self.paginate_posts(posts, page_number)
+
         post_form = PostForm()
         users = self.request.user.get_unfollowed_users()
-        return render(request, self.template_name, {'posts': posts, 'post_form': post_form, 'users': users})
+        return render(request, self.template_name, {
+            'posts': paginated_posts,
+            'post_form': post_form,
+            'users': users
+        })
 
     def post(self, request, *args, **kwargs):
         if request.POST.get('form_type') == 'post_form':
@@ -35,9 +49,15 @@ class PostMainView(PostsMixin, View):
             post_form = PostForm()
 
         posts = self.get_posts()
-        users = self.request.user.get_unfollowed_users()
-        return render(request, self.template_name, {'posts': posts, 'post_form': post_form, 'users': users})
+        page_number = request.GET.get('page', 1)
+        paginated_posts = self.paginate_posts(posts, page_number)
 
+        users = self.request.user.get_unfollowed_users()
+        return render(request, self.template_name, {
+            'posts': paginated_posts,
+            'post_form': post_form,
+            'users': users
+        })
 
 class PostDetailView(PostsMixin, DetailView):
     model = Post
