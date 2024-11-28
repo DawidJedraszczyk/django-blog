9150dac41d2906d82a19a3a26009e36169e79a1e
DawidJedraszczyk <dawidjedraszczyk01@gmail.com>
2024-11-29 00:53:05 +0100

    urls translations

--- blog/urls.py
+++ blog/urls.py
@@ -21,19 +21,23 @@ from django.conf.urls.static import static
 from allauth.account.views import SignupView, LoginView, LogoutView, PasswordChangeView, PasswordResetView
 from django.utils.translation import gettext_lazy as _
 from django.views.i18n import set_language
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('set_language/', set_language, name='set_language'),
+    path('', include('apps.posts.urls')),
+]
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path(_('accounts/'), include('allauth.urls')),
     path(_('users/'), include('apps.users.urls')),
-    path(_('set_language/'), set_language, name='set_language'),
-    path('', include('apps.posts.urls')),
     path(_('accounts/signup/'), SignupView.as_view(), name='account_signup'),
     path(_('accounts/login/'), LoginView.as_view(), name='account_login'),
     path(_('accounts/logout/'), LogoutView.as_view(), name='account_logout'),
     path(_('accounts/password/change/'), PasswordChangeView.as_view(), name='account_change_password'),
     path(_('accounts/password/reset/'), PasswordResetView.as_view(), name='account_reset_password'),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
